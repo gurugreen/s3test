16d4258ce941e1bc4fe13063f108189f1d1d7142
gurugreen <guruprasanna.green@gmail.com>
2024-07-17 22:23:16 +0530

    update 2

--- s3.tf
+++ s3.tf
@@ -1,11 +1,9 @@
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "aws2024ac-17072024"
-  acl    = "private"
 }
 
 resource "aws_s3_bucket_object" "my_object" {
   bucket = aws_s3_bucket.my_bucket.bucket
   key    = "./test.txt"
   source = "folders/file.txt"
-  acl    = "private"
 }
