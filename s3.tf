afacfc43525f5b793df8179c70e8b11c7338f47f
gurugreen <guruprasanna.green@gmail.com>
2024-07-17 22:25:08 +0530

    update 3

--- s3.tf
+++ s3.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket" "my_bucket" {
   bucket = "aws2024ac-17072024"
 }
 
-resource "aws_s3_bucket_object" "my_object" {
+resource aws_s3_object "my_object" {
   bucket = aws_s3_bucket.my_bucket.bucket
   key    = "./test.txt"
   source = "folders/file.txt"
